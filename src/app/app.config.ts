bcab286e30c3eb427998e75b0c11b4ec70e28406
patrykzawadzkisggw <115794809+patrykzawadzkisggw@users.noreply.github.com>
2025-10-25 14:58:43 +0000

    - Added PrimeNG dependencies - Added Tailwind CSS

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,26 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimationsAsync(),
+        providePrimeNG({
+            theme: {
+                preset: Aura,
+        options: {
+          prefix: 'p', // domyślnie
+          darkModeSelector: '.app-dark',
+        }
+            }
+        })
   ]
 };
--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [CardModule,ButtonModule, DialogModule, InputTextModule],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss'
 })
 export class HomePage {
+visible: boolean = false;
 
+  showDialog(): void {
+    this.visible = true;
+  }
 }
