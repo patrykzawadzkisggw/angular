0b7a97f6e346ff6d12945791bd44564cebf60e46
JustNormalProgrammer <testkonto1290@gmail.com>
2025-11-02 16:58:48 +0100

    Add initial version of login/register forms

--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -1,11 +1,64 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormsModule,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { MessageModule } from 'primeng/message';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth-service';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [
+    FormsModule,
+    InputTextModule,
+    ButtonModule,
+    CardModule,
+    ReactiveFormsModule,
+    FloatLabelModule,
+    MessageModule,
+  ],
   templateUrl: './login-page.html',
-  styleUrl: './login-page.scss'
 })
 export class LoginPage {
+  isInvalid(controlName: string): boolean {
+    const control = this.loginForm.get(controlName);
+    return !!control && control.invalid && control.touched;
+  }
 
+  constructor(private auth: AuthService, private router: Router) {}
+
+  loginForm = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+
+  login() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    const { username, password } = this.loginForm.value;
+
+    if (!username || !password) return;
+
+    this.auth.login(username, password).subscribe({
+      next: () => {
+        this.router.navigate(['/']);
+        console.log('success');
+      },
+      error: (err) => {
+        console.error('Login error:', err);
+        console.error('Error route hit');
+      },
+    });
+  }
 }
--- src/app/pages/register-page/register-page.ts
+++ src/app/pages/register-page/register-page.ts
@@ -1,11 +1,82 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormsModule,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+import { MessageModule } from 'primeng/message';
+import { AuthService } from '../../services/auth-service';
+import { Router } from '@angular/router';
+
+function passwordMatchValidator(control: AbstractControl): ValidationErrors | null {
+  const passControl = control.get('password');
+  const confirmControl = control.get('confirmPassword');
+
+  if (!passControl || !confirmControl) return null;
+
+  if (passControl.value !== confirmControl.value) {
+    confirmControl.setErrors({ passwordMismatch: true });
+    return { passwordMismatch: true };
+  } else {
+    if (confirmControl.hasError('passwordMismatch')) {
+      confirmControl.setErrors(null);
+    }
+    return null;
+  }
+}
 
 @Component({
   selector: 'app-register-page',
-  imports: [],
+  imports: [
+    FormsModule,
+    InputTextModule,
+    ButtonModule,
+    CardModule,
+    ReactiveFormsModule,
+    FloatLabelModule,
+    MessageModule,
+  ],
   templateUrl: './register-page.html',
-  styleUrl: './register-page.scss'
 })
 export class RegisterPage {
+  constructor(private auth: AuthService, private router: Router) {}
+  isInvalid(controlName: string): boolean {
+    const control = this.registerForm.get(controlName);
+    return !!control && control.invalid && control.touched;
+  }
+  registerForm = new FormGroup(
+    {
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+      confirmPassword: new FormControl('', Validators.required),
+    },
+    { validators: passwordMatchValidator }
+  );
 
+  register() {
+    if (this.registerForm.invalid) {
+      this.registerForm.markAllAsTouched();
+      return;
+    }
+
+    const { username, password } = this.registerForm.value;
+
+    if (!username || !password) return;
+
+    this.auth.register(username, password).subscribe({
+      next: () => {
+        this.router.navigate(['/login']);
+      },
+      error: (err) => {
+        console.error('Register error:', err);
+      },
+    });
+  }
 }
