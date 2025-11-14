f8a95c3fbb5b69ff74639e95463b14c98d484da2
JustNormalProgrammer <testkonto1290@gmail.com>
2025-11-14 14:14:29 +0100

    Add request interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,19 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
-
 import { routes } from './app.routes';
+import { authInterceptor } from './auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
     provideAnimationsAsync(),
         providePrimeNG({
             theme: {
---
+++ src/app/auth-interceptor.ts
@@ -0,0 +1,32 @@
+import { HttpErrorResponse, HttpHandlerFn, HttpRequest } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+import { AuthService } from './services/auth-service';
+import { Router } from '@angular/router';
+import { inject } from '@angular/core';
+
+export function authInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn) {
+  const token = inject(AuthService).getToken();
+  const router = inject(Router);
+
+  if (token) {
+    console.log('Token injected');
+    request = request.clone({
+      setHeaders: { Authorization: `Bearer ${token}` },
+    });
+  } else {
+    console.log('No token found');
+   }
+
+  return next(request).pipe(
+    catchError((err) => {
+      if (err instanceof HttpErrorResponse) {
+        if (err.status === 401) {
+          router.navigate(['/login'], {
+            queryParams: { redirect: router.routerState.snapshot.url },
+          });
+        }
+      }
+      return throwError(() => err);
+    })
+  );
+}
